5419ce020b4d0c09e35393da400a7fb82b1c8782
Hubert Czobodziński <hcz@onet.eu>
2012-04-04 01:36:18 +0200

    Number theory module.

---
+++ number_theory.py
@@ -0,0 +1,15 @@
+# vim: set fileencoding=utf-8 :
+
+
+def gcd(a, b):
+    """
+    Greatest common divisor -- Euclidean algorithm.
+    """
+    pass
+
+
+def lcm(a, b):
+    """
+    Least common multiple.
+    """
+    return a * b / gcd(a, b)
