8cf386503fb8cb71a5351e91b98bd9d8d79698b3
Hubert Czobodziński <hcz@onet.eu>
2012-04-04 13:37:14 +0200

    gcd and lcm functions.

--- number_theory.py
+++ number_theory.py
@@ -4,12 +4,20 @@
 def gcd(a, b):
     """
     Greatest common divisor -- Euclidean algorithm.
+    >>> gcd(0, 5)
+    5
+    >>> gcd(1071, 1029)
+    21
     """
-    pass
+    while b != 0:
+        a, b = b, a % b
+    return a
 
 
 def lcm(a, b):
     """
     Least common multiple.
+    >>> lcm(1071, 1029)
+    52479
     """
-    return a * b / gcd(a, b)
+    return a * b // gcd(a, b)
