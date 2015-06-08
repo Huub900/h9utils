37760a3c27c33ef09e3301fc79e0917741fdebdf
Hubert Czobodziński <hcz@onet.eu>
2015-06-08 21:28:54 +0200

    Fibonacci number.

--- number_theory.py
+++ number_theory.py
@@ -12,3 +12,13 @@ def gcd(a, b):
 def lcm(a, b):
     """Least common multiple."""
     return a * b // gcd(a, b)
+
+
+def fib(n):
+    """Fibonacci number."""
+    if n == 0: return 0
+    if n == 1: return 1
+    a, b = 0, 1
+    for i in range(2, n + 1):
+        a, b = b, a+b
+    return b
--- test_number_theory.py
+++ test_number_theory.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from number_theory import gcd, lcm
+from number_theory import gcd, lcm, fib
 
 
 class TestGcd(TestCase):
@@ -21,3 +21,16 @@ class TestLcm(TestCase):
     def test_other(self):
         self.assertEqual(lcm(1071, 1029), 52479)
 
+
+class TestFib(TestCase):
+    def test_zero(self):
+        self.assertEqual(fib(0), 0)
+
+    def test_one(self):
+        self.assertEqual(fib(1), 1)
+
+    def test_nine(self):
+        self.assertEqual(fib(9), 34)
+
+    def test_nineteen(self):
+        self.assertEqual(fib(19), 4181)
